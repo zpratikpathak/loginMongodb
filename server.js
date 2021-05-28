bac9715a8469dccab1dae6c35edb5de601c42d2e
Pratik Pathak <pratikpathak200@gmail.com>
2021-05-28 12:02:18 +0530

    Received data in Backend

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser"); // to parse the JSON POSTED data from web
 
 const app = express();
 app.use("/", express.static(path.join(__dirname, "static")));
+
+app.use(bodyParser.json()); //middleware
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
+});
 
 const port = 3500;
 
